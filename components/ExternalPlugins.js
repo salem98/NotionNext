f0f58a24d75514060c36d1e28cf4dfc72f9cf28a
salemgo <gtavuilam@gmail.com>
2023-10-29 02:31:14 +0800

    drag

--- components/ExternalPlugins.js
+++ components/ExternalPlugins.js
@@ -15,7 +15,7 @@ import WebWhiz from './Webwhiz'
 
 const TwikooCommentCounter = dynamic(() => import('@/components/TwikooCommentCounter'), { ssr: false })
 const DebugPanel = dynamic(() => import('@/components/DebugPanel'), { ssr: false })
-//const ThemeSwitch = dynamic(() => import('@/components/ThemeSwitch'), { ssr: false })
+const ThemeSwitch = dynamic(() => import('@/components/ThemeSwitch'), { ssr: false })
 const Fireworks = dynamic(() => import('@/components/Fireworks'), { ssr: false })
 const Nest = dynamic(() => import('@/components/Nest'), { ssr: false })
 const FlutteringRibbon = dynamic(() => import('@/components/FlutteringRibbon'), { ssr: false })
@@ -37,10 +37,10 @@ const AdBlockDetect = dynamic(() => import('@/components/AdBlockDetect'), { ssr:
  * 各种第三方组件
  * @param {*} props
  * @returns
- * {JSON.parse(BLOG.THEME_SWITCH) && <ThemeSwitch />}
  */
 const ExternalPlugin = (props) => {
   return <>
+          {JSON.parse(BLOG.THEME_SWITCH) && <ThemeSwitch />}
           {JSON.parse(BLOG.DEBUG) && <DebugPanel />}
           {BLOG.ANALYTICS_ACKEE_TRACKER && <Ackee />}
           {BLOG.ANALYTICS_GOOGLE_ID && <Gtag />}
--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -4,17 +4,12 @@ import { Draggable } from './Draggable'
 import { THEMES } from '@/themes/theme'
 import { useRouter } from 'next/router'
 import DarkModeButton from './DarkModeButton'
-/**
- *
- * @returns 主题切换
- */
+
 const ThemeSwitch = () => {
   const { theme } = useGlobal()
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
 
-  // 修改当前路径url中的 theme 参数
-  // 例如 http://localhost?theme=hexo 跳转到 http://localhost?theme=newTheme
   const onSelectChange = (e) => {
     setIsLoading(true)
     const newTheme = e.target.value
@@ -25,29 +20,46 @@ const ThemeSwitch = () => {
     })
   }
 
-  return (<>
-        <Draggable>
-            <div id="draggableBox" style={{ left: '10px', top: '80vh' }} className="fixed z-50 dark:text-white bg-gray-50 dark:bg-black rounded-2xl drop-shadow-lg">
-                <div className="p-3 w-full flex items-center text-sm group duration-200 transition-all">
-                    <DarkModeButton className='mr-2' />
-                    <div className='w-0 group-hover:w-20 transition-all duration-200 overflow-hidden'>
-                        <select value={theme} onChange={onSelectChange} name="themes" className='appearance-none outline-none dark:text-white bg-gray-50 dark:bg-black uppercase cursor-pointer'>
-                            {THEMES?.map(t => {
-                              return <option key={t} value={t}>{t}</option>
-                            })}
-                        </select>
-                    </div>
-                    <i className="fa-solid fa-palette pl-2"></i>
-                </div>
-            </div>
-            {/* 切换主题加载时的全屏遮罩 */}
-            <div className={`${isLoading ? 'opacity-50 ' : 'opacity-0'} w-screen h-screen bg-black text-white shadow-text flex justify-center items-center
-                              transition-all fixed top-0 left-0 pointer-events-none duration-1000 z-50 shadow-inner`}>
-                <i className='text-3xl mr-5 fas fa-spinner animate-spin' />
-            </div>
-        </Draggable>
-    </>
-  )
+  // Check if THEME_SWITCH_DRAG is true in blog.config.js
+  const isDraggable = process.env.THEME_SWITCH_DRAG === 'true'
+
+  // Conditionally render based on the value of isDraggable
+  return isDraggable ? (
+    <Draggable>
+      <div
+        id="draggableBox"
+        style={{ left: '10px', top: '80vh' }}
+        className="fixed z-50 dark:text-white bg-gray-50 dark:bg-black rounded-2xl drop-shadow-lg"
+      >
+        <div className="p-3 w-full flex items-center text-sm group duration-200 transition-all">
+          <DarkModeButton className="mr-2" />
+          <div className="w-0 group-hover:w-20 transition-all duration-200 overflow-hidden">
+            <select
+              value={theme}
+              onChange={onSelectChange}
+              name="themes"
+              className="appearance-none outline-none dark:text-white bg-gray-50 dark:bg-black uppercase cursor-pointer"
+            >
+              {THEMES?.map((t) => {
+                return (
+                  <option key={t} value={t}>
+                    {t}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+          <i className="fa-solid fa-palette pl-2"></i>
+        </div>
+      </div>
+      <div
+        className={`${isLoading ? 'opacity-50 ' : 'opacity-0'} w-screen h-screen bg-black text-white shadow-text flex justify-center items-center
+                              transition-all fixed top-0 left-0 pointer-events-none duration-1000 z-50 shadow-inner`}
+      >
+        <i className="text-3xl mr-5 fas fa-spinner animate-spin" />
+      </div>
+    </Draggable>
+  ) : null
 }
 
 export default ThemeSwitch
