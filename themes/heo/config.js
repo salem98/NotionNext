0a1c2a389858fcd0379eef91dfb9d8c170566c6f
salemgo <gtavuilam@gmail.com>
2023-10-29 01:47:27 +0800

    update

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,25 +5,25 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'Nhận mentor theo tháng!', url: 'https://nathanpham.dev },
+    { title: 'Tham gia nhóm Telegram DevOps Việt Nam', url: 'https://nathanpham.dev' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: 'Chia sẻ',
+  HERO_TITLE_2: 'DevOps',
+  HERO_TITLE_3: 'nathanpham.dev',
+  HERO_TITLE_4: 'Bài viết mới',
+  HERO_TITLE_5: 'NotionNext4.0',
+  HERO_TITLE_LINK: '#',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: 'DevOps', url: '/tag/devops' },
+  HERO_CATEGORY_2: { title: 'Popular', url: '/tag/popular' },
+  HERO_CATEGORY_3: { title: 'Tutorial', url: '/tag/tutorial' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HERO_RECOMMEND_POST_TAG: '推荐',
+  HERO_RECOMMEND_POST_TAG: 'Recommend',
   HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
